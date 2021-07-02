a99fc7a75f768dee67472b9363feb9e74e5aeb45
Lilypy <52183009+BckLily@users.noreply.github.com>
2021-07-02 17:22:50 +0900

    Object Move Crash
    Game Object's Transform Translate Function has Passing the Wall Object.
    Try to Fix this.
    ongoing......

--- 3DShooting/Assets/02.Scripts/Player/PlayerCtrlGetAxis.cs
+++ 3DShooting/Assets/02.Scripts/Player/PlayerCtrlGetAxis.cs
@@ -15,22 +15,29 @@ public class PlayerCtrlGetAxis : MonoBehaviour
     private float moveSpeed = 10.0f;    // Player의 이동 속도
     private float rotSpeed = 120.0f;    // Player의 회전 속도
 
+    private Rigidbody rb;               // Player의 Rigidbody
+    bool canMove;                       // Player의 이동가능 여부
 
+    Vector3 center;                     // Player의 중심부
+    Vector3 moveDir;
 
     // Start is called before the first frame update
     void Start()
     {
 
         // Transform 위치 저장.
-        tr = GetComponent<Transform>();
+        tr = gameObject.GetComponent<Transform>();
 
+        rb = gameObject.GetComponent<Rigidbody>();
 
-
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        center = tr.position + tr.up * 1.0f;
+
+        RaycastHit hit;
 
 
         horizontal = Input.GetAxis("Horizontal");
@@ -38,20 +45,76 @@ public class PlayerCtrlGetAxis : MonoBehaviour
         h_rotate = Input.GetAxis("Mouse X");
         //v_rotate = Input.GetAxis("Mouse Y");
 
-        // 전후 좌우 이동 방향 벡터 계산
-        Vector3 moveDir = (Vector3.forward * vertical) + (Vector3.right * horizontal);
 
-        // Translate(이동 방향 * 속도 * 변위 값 * Time.deltaTime, 기준 좌표)
-        tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+
+        // 전후 좌우 이동 방향 벡터 계산
+        moveDir = new Vector3(horizontal, 0, vertical);
+        //Vector3 moveDir = (Vector3.forward * vertical) + (Vector3.right * horizontal);
+
+
+        bool sphereCast = Physics.SphereCast(center, 0.35f, moveDir, out hit, 1 << 7);
+
+
+        if (moveDir == Vector3.zero)
+        {
+            canMove = false;
+        }
+        else if(moveDir != Vector3.zero && sphereCast == true)
+        {
+            canMove = true;
+        }
+
+
+
+        if (canMove)
+        {
+
+            // Translate(이동 방향 * 속도 * 변위 값 * Time.deltaTime, 기준 좌표)
+            tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+
+        }
 
         // Vector3.up 축을 기준으로 rotSpeed 만큼의 속도로 회전
         tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * h_rotate);
+
+
+        Debug.Log(tr.transform.position);
+        Debug.Log("Center: " + center);
+        Debug.Log("moveDir: " + moveDir);
+        Debug.Log("Cast: " + sphereCast);
+        Debug.Log("canMove: " + canMove);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
 
 
 
 
 
     }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(center, 0.3f);
+
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log("COLL ENTER");
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.name=="Wall")
+    //    Debug.Log("TRIG ENTER");
+    //}
 
 
 }
